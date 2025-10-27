dda88495fe3a228aa2c90955124efe9987d9a5a3
Rayberk <68689359+Rayberk@users.noreply.github.com>
2025-10-27 18:15:53 +0400

    Update scene structure and editor layout
    Refactored the scene hierarchy in the autosave file, introducing new parent objects and reorganizing entities for clarity. Updated physics and collision properties for several objects. Adjusted .editor/imgui.ini and .editor/state.json to reflect new window positions, sizes, and enabled states for Project Settings and Resources. Updated cached editor bundle and main project file to match these changes.

--- cache/project/js/_editor_index.js
+++ cache/project/js/_editor_index.js
@@ -1,4 +1,4 @@
 import * as __wonderlandengine_components from '@wonderlandengine/components';
 _registerEditor(__wonderlandengine_components);
-import * as __Users_nyuad_WonderlandEngine_VR_Balance_Mobility_Trainer_js_button_js from '/Users/nyuad/WonderlandEngine/VR-Balance-Mobility-Trainer/js/button.js';
-_registerEditor(__Users_nyuad_WonderlandEngine_VR_Balance_Mobility_Trainer_js_button_js);
+import * as __Volumes_SAVE_HERE_RC5984_github_VR_Balance_Mobility_Trainer_js_button_js from '/Volumes/SAVE HERE/RC5984/github/VR-Balance-Mobility-Trainer/js/button.js';
+_registerEditor(__Volumes_SAVE_HERE_RC5984_github_VR_Balance_Mobility_Trainer_js_button_js);
